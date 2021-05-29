05b9ed876a99a55cfc33020be9d636777cf409b3
Vladislav Radchenko <specforworkwithpeop@gmail.com>
2021-05-29 23:08:55 +0300

    Change structure and tests

--- dz.py
+++ dz.py
@@ -1,71 +1,38 @@
 def data():
-    print('Enter a, b and c')
-    a = float(input())
-    b = float(input())
-    c = float(input())
+    a,b,c = map(float, input().split())
     return a,b,c
 
-def discriminant(a,b,c):
-    d = (b ** 2) - (4 * a * c)
-    return d
-
-def first_case(a,b,d):
-    'If D > 0'
-    x1 = ((-1 * b) + (d ** 0.5)) / (2 * a)
-    x2 = ((-1 * b) - (d ** 0.5)) / (2 * a)
-    return x1,x2
-
-def second_case(a,b):
-    'If D = 0'
-    x1 = x2 = -1 * (b / (2 * a))
-    return x1,x2
-
-def third_case():
-    'If D < 0'
+def decision(a,b,c):
     x1 = x2 = 0
-    print('No radix')
-
-def fourth_case():
-    'If b = 0 and c = 0'
-    print('First radix = 0\nSecond radix = 0')
-
-def fifth_case(a,c):
-    'If b = 0 and c != 0'
-    if (-1 * (c / a)) > 0:
-        x1 = (-1 * (c / a)) ** 0.5
-        x2 = -1 * ((-1 * (c / a)) ** 0.5)
+    d = (b ** 2) - (4 * a * c)
+    if b == 0 and c == 0:
+        print('First radix = 0\nSecond radix = 0')
+    elif b == 0 and c != 0:
+        if (-1 * (c / a)) > 0:
+            x1 = (-1 * (c / a)) ** 0.5
+            x2 = -1 * ((-1 * (c / a)) ** 0.5)
+        else:
+            x1 = x2 = 0
+            print('No radix')
+    elif b != 0 and c == 0:
+        x1 = 0
+        x2 = -1 * (b / a)
     else:
-        x1 = x2 = 0
-        print('No radix')
-    return x1,x2
-
-def sixth_case(a,b):
-    'If b != 0 and c = 0'
-    x1 = 0
-    x2 = -1 * (b / a)
+        if d > 0:
+            x1 = ((-1 * b) + (d ** 0.5)) / (2 * a)
+            x2 = ((-1 * b) - (d ** 0.5)) / (2 * a)
+        elif d == 0:
+            x1 = x2 = -1 * (b / (2 * a))
+        else:
+            x1 = x2 = 0
+            print('No radix')
+    if x1 and x2 != 0:
+        print(f'First radix = {x1}\nSecond radix = {x2}')
     return x1,x2
 
 def main():
-    x1 = x2 = 0
     a,b,c = data()
-    if b == 0 and c == 0:
-        fourth_case()
-    elif b == 0 and c != 0:
-        x1,x2 = fifth_case(a,c)
-    elif b != 0 and c == 0:
-        x1,x2 = sixth_case(a,b)
-    else:
-        d = discriminant(a,b,c)
-        if d > 0:
-            x1,x2 = first_case(a,b,d)
-        elif d == 0:
-            x1,x2 = second_case(a,b)
-        else:
-            third_case()
-    if x1 and x2 != 0:
-        print(f'First radix = {x1}\nSecond radix = {x2}')
-    
-    
+    x1,x2 = decision(a,b,c)
 
 if __name__ == '__main__':
     main()
--- utest_dz.py
+++ utest_dz.py
@@ -3,17 +3,14 @@ import dz
 
 
 class DzTest(unittest.TestCase):
-    def test_first(self):
-        self.assertEqual(dz.first_case(3,-14,256), (5.0, -0.3333333333333333))
+    def test(self):
+        self.assertEqual(dz.decision(3,-14,-5), (5.0, -0.3333333333333333)) # D > 0
+        self.assertEqual(dz.decision(4,-20,25), (2.5, 2.5)) # D = 0
+        self.assertEqual(dz.decision(-3,0,75), (5, -5)) # b = 0
+        self.assertEqual(dz.decision(4,12,0), (0, -3)) # c = 0 
+        self.assertEqual(dz.decision(1,0,0), (0, 0)) # b = 0, c = 0
 
-    def test_second(self):
-        self.assertEqual(dz.second_case(4,-20),(2.5,2.5))
     
-    def test_fifth(self):
-        self.assertEqual(dz.fifth_case(4,-64),(4,-4))
-    
-    def test_sixth(self):
-        self.assertEqual(dz.sixth_case(4,12),(0,-3))
 
 if __name__ == '__main__':
     unittest.main()
